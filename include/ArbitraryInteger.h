26d936060c683edaa30897ed3dd0b2419bdd29c0
Anton Todua <toduaanton@yandex.ru>
2015-09-20 00:16:58 +0400

    remove wrong static_assert

--- include/ArbitraryInteger.h
+++ include/ArbitraryInteger.h
@@ -7,16 +7,12 @@ namespace Refal2 {
 //-----------------------------------------------------------------------------
 // CArbitraryInteger
 
-#ifndef UINT32_MAX
-static_assert( false, "minimum 32 unsigned integer number required" )
-#endif
-
 class CArbitraryInteger : private std::vector<uint32_t> {
 public:
 	typedef value_type TDigit;
 	typedef size_type TDigitIndex;
 	static const TDigit Base = 1 << 24;
-	static_assert( UINT32_MAX / 2 >= Base, "too big Base");
+	static_assert( Base <= 0x0FFFFFFF, "too big Base");
 
 	CArbitraryInteger() { Zero(); }
 	explicit CArbitraryInteger( TDigit digit );
